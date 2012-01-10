ef01bc879db7f20b317171e62245e1d37f415b03
Ian Rudie <ilrudie@gmail.com>
2012-01-10 10:19:14 -0500

    improved version of hbaDetails.sh
    imporved modularity and error handling in hbaDetails
    created new functions to replace code used multiple times
    created standard format templtes for the details to ensure columns line up

--- script/hbaDetails.sh
+++ script/hbaDetails.sh
@@ -9,12 +9,18 @@
 ##	1.0 - basic functionality and emulex detail
 ##		error handling
 ##		2012/1/5 - IR
+##
+##	1.1 - imporved modularity and design
+##		2012/1/10 - IR
 
 ##BEGIN EMULEX DETAIL
 #@FUNCTION emulexDetail
 #@USAGE emulexDetail $HBA
 #@ARGUMENT $HBA - the name of the directoy under /sys/class/fc_host/ which represents an HBA port
 emulexDetail () { 
+
+#create emulex detail header format string
+EDHEADFORMAT="%12s %4s %6s %3s %8s %14s"
 
 #catch faulty calls with no argument
 if [ ! $1 ] 
@@ -28,7 +34,7 @@ then
 elif [ $1 =  "HEADER" ]
 then
 	#print header
-	printf "%12s %4s %6s %3s %8s %14s" "Model" "LQD" "QD" "TMO" "FW Rev" "Driver" 2>/dev/null
+	printf "$EDHEADFORMAT" "Model" "LQD" "QD" "TMO" "FW Rev" "Driver" 2>/dev/null
 	return
 #catch if argument supplied is actually a valid HBA
 elif [ ! -d /sys/class/scsi_host/${1}  ]
@@ -57,16 +63,37 @@ FWREV=`cat /sys/class/scsi_host/${HBA}/fwrev | awk '{print $1}' 2>/dev/null`
 DRREV=`cat /sys/class/scsi_host/${HBA}/lpfc_drvr_version | awk '{print $NF}' 2>/dev/null`
 
 #print emulex detail
-printf "%12s %4s %6s %3s %8s %14s" $MODELNAME $LQD $QD $TMO $FWREV $DRREV 2>/dev/null
+printf "$EDHEADFORMAT" $MODELNAME $LQD $QD $TMO $FWREV $DRREV 2>/dev/null
 
 }
 ##END EMULEX DETAIL
 
-##BEGIN MAIN
-#print standard header
-printf "%6s %24s %8s %8s" "HBA" "WWPN" "State" "Speed" 2>/dev/null
-#detect emulex cards
+##BEGIN DETECT EMULEX
+#@FUNCTION detectEmulex
+#@USAGE detectEmulex
+#@NOARGUMENT
+
+detectEmulex () {
+
+#look for files with lpfc (Light Pulse Fibre Channel)
 if [[ 0 -lt $(ls /sys/class/scsi_host/host*/ | grep lpfc | wc -l 2>/dev/null) ]]
+then
+	return $(true)
+else
+	return $(false)
+fi
+
+}
+##END DETECT EMULEX
+
+##BEGIN MAIN
+#create standard header format string
+SHEADFORMAT="%6s %24s %8s %6s"
+#print standard header
+printf "$SHEADFORMAT" "HBA" "WWPN" "State" "Speed" 2>/dev/null
+
+#detect emulex cards
+if detectEmulex
 then
 	#print emulex header
 	emulexDetail "HEADER"
@@ -88,9 +115,9 @@ do
 	#get HBA speed
 	SPEED=`cat /sys/class/fc_host/${HBA}/speed | awk '{print $1}' 2>/dev/null`
 	#print standard information
-	printf "%6s %24s %8s %8s" $HBA $WWN $STATE $SPEED 2>/dev/null
+	printf "$SHEADFORMAT" $HBA $WWN $STATE $SPEED 2>/dev/null
 	#detect emulex cards
-	if [[ 0 -lt $(ls /sys/class/scsi_host/host*/ | grep lpfc | wc -l 2>/dev/null) ]]	
+	if detectEmulex
 	then
 		#print the details output for this emulex HBA
 		emulexDetail $HBA
